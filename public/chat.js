846fb48b04d5a1d98833f6f32a2d141fc2ac86c4
scrivy <scrivinator@gmail.com>
2013-10-09 00:03:23 -0700

    hmm

--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,12 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://192.168.1.9:3700');
+    var socket = io.connect('http://pssst.herokuapp.com');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
     var name = document.getElementById("name");
+    password = prompt("you're a weasel");
 
     socket.on('message', function (data) {
         if(data.message) {
@@ -15,7 +16,7 @@ window.onload = function() {
             for(var i=0; i<messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
 
-                var decrypted = CryptoJS.AES.decrypt(messages[i].message, "heyheyhey");
+                var decrypted = CryptoJS.AES.decrypt(messages[i].message, password);
                 html += decrypted.toString(CryptoJS.enc.Utf8) + '<br />';
             }
             content.innerHTML = html;
@@ -29,7 +30,7 @@ window.onload = function() {
         if(name.value == "") {
             alert("Please type your name!");
         } else {
-            var encrypted = CryptoJS.AES.encrypt(field.value, "heyheyhey");
+            var encrypted = CryptoJS.AES.encrypt(field.value, password);
             console.log('sending - ' + encrypted.toString());
             socket.emit('send', { message: encrypted.toString(), username: name.value });
             field.value = "";
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var Express = require("express");
 var app = Express();
-var port = 3700;
+var port = process.env.PORT || 3700;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
@@ -16,10 +16,14 @@ app.use(Express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+io.configure(function() {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 10);
+});
+
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
 });
-
