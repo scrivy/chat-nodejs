2ee4a4158c57f420a516ee4ef866f60118048068
scrivy <scrivinator@gmail.com>
2013-10-09 00:30:49 -0700

    disabled server message on connection open message

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+//    socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
