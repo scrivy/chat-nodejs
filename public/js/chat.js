d1c5f7dc85f5a38e6f5239c10f0b090c8a364ee9
scrivy <scrivinator@gmail.com>
2013-10-18 14:49:08 -0700

    changed websocket address

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 
     var messages = [];
-    var socket = io.connect('http://www.psst.in');
+    var socket = io.connect('https://www.psst.in');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
