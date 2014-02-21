7fd1b6c1a1f0b574e6c8c2bbfd69feb4834383b4
scrivy <scrivinator@gmail.com>
2014-02-21 01:09:21 -0800

    tweaked some stuff

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,93 +1,77 @@
-(function(){
+var socket = io.connect('');
 
-  var socket = io.connect('');
+var LobbyView = Backbone.View.extend({
+  events: {
+    "click    #send"          : "sendmessage",
+    "keypress input#field"    : "sendmessageonenter"
+  },
 
-  var LobbyView = Backbone.View.extend({
-    el: 'body',
+  initialize: function() {
+    _.bindAll(this, 'sendmessage');
 
-    events: {
-      "click    #connect"       : "connecttoserver",
-      "keypress input#password" : "connectonpasswordenter",
-      "click    #send"          : "sendmessage",
-      "keypress input#field"    : "sendmessageonenter"
-    },
+    var that = this;
 
-    initialize: function() {
-      _.bindAll(this, 'sendmessage');
-
-      var that = this;
-
-      // chat session setup stuff
-      $('#setup')
-        .modal({
-          closable: false,
-          onHide: function(){
-            that.model.set({
-              name: $('#name').val(),
-              key: CryptoJS.SHA256($('#password').val()).toString()
-            });
-          }
-        })
-        .modal('show')
-      ;
-
-      // send message setup stuff
-      socket.on('message', function(data) {
-        if (data) {
-          console.log('received - ' + data);
-
-          try {
-            var message = JSON.parse(CryptoJS.AES.decrypt(data, that.model.get('key')).toString(CryptoJS.enc.Utf8))
-              , contentDIV = $('#content');
-
-            var html = '<b>' + message.username + ': </b>';
-            html += message.message + '<br />';
-
-            contentDIV.append(html);
-
-            contentDIV[0].scrollTop = contentDIV[0].scrollHeight;
-          } catch(err) {
-            console.log('Error decrypting, probably wrong key - ' + err);
-          }
+    // chat session setup stuff
+    $('#setup')
+      .modal({
+        closable: false,
+        onApprove: function(){
+          that.model.set({
+            name: $('#name').val(),
+            key: CryptoJS.SHA256($('#password').val()).toString()
+          });
         }
-      });
+      })
+      .modal('show')
+    ;
 
-      socket.on('people', function(data) {
-        this.peoplecount = data.count;
+    // send message setup stuff
+    socket.on('message', function(data) {
+      if (data) {
+        console.log('received - ' + data);
 
-        document.getElementById("peoplecount").innerHTML = this.peoplecount;
-      });
-    },
+        try {
+          var message = JSON.parse(CryptoJS.AES.decrypt(data, that.model.get('key')).toString(CryptoJS.enc.Utf8))
+            , contentDIV = $('#content');
 
-    connecttoserver: function() {
-      $('#setup').modal('hide');
-    },
+          var html = '<b>' + message.username + ': </b>';
+          html += message.message + '<br />';
 
-    sendmessage: function() {
-      var message = {
-        message: $('#field').val(),
-        username: this.model.get('name')
-      };
-      var encrypted = CryptoJS.AES.encrypt(JSON.stringify(message), this.model.get('key')).toString();
-      console.log('sending - ' + encrypted);
-      socket.emit('send', encrypted);
-      $('#field').val('');
-    },
+          contentDIV.append(html);
 
-    connectonpasswordenter: function(e) {
-      if (e.keyCode == 13)
-        this.connecttoserver();
-    },
+          contentDIV[0].scrollTop = contentDIV[0].scrollHeight;
+        } catch(err) {
+          console.log('Error decrypting, probably wrong key - ' + err);
+        }
+      }
+    });
 
-    sendmessageonenter: function(e) {
-      if (e.keyCode == 13)
-        this.sendmessage();
-    }
+    socket.on('people', function(data) {
+      this.peoplecount = data.count;
 
-  });
+      document.getElementById("peoplecount").innerHTML = this.peoplecount;
+    });
+  },
 
-  var App = new LobbyView({
-    model: new Backbone.Model()
-  });
+  sendmessage: function() {
+    var message = {
+      message: $('#field').val(),
+      username: this.model.get('name')
+    };
+    var encrypted = CryptoJS.AES.encrypt(JSON.stringify(message), this.model.get('key')).toString();
+    console.log('sending - ' + encrypted);
+    socket.emit('send', encrypted);
+    $('#field').val('');
+  },
 
-})();
+  sendmessageonenter: function(e) {
+    if (e.keyCode == 13)
+      this.sendmessage();
+  }
+
+});
+
+var app = new LobbyView({
+  el: document.getElementById('lobby'),
+  model: new Backbone.Model()
+});
