457072fb3e2d92e3ad4b496207dac6d6ae73adbe
scrivy <scrivinator@gmail.com>
2014-06-08 21:49:42 -0700

    added autoscrolldown directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,3 +1,13 @@
 'use strict';
 
-
+chat.directive('autoscrolldown', function($timeout) {
+  return {
+    link: function(scope, element, attrs) {
+      scope.$watch(function() {
+        return element[0].scrollHeight;
+      }, function(newval, oldval) {
+        element[0].scrollTop = newval;
+      });
+    }
+  }
+})
